7d0e3bbf43b47e9735644d8b431f6ccaea658429
Ilya Chernyshov <34981418+ilya449@users.noreply.github.com>
2020-09-14 17:42:40 +0300

    Update src/main/java/com/internet/shop/controller/admin/AdminOrderController.java
    Co-authored-by: dmytro-hryhoruk <50640794+dmytro-hryhoruk@users.noreply.github.com>

--- src/main/java/com/internet/shop/controller/admin/AdminOrderController.java
+++ src/main/java/com/internet/shop/controller/admin/AdminOrderController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/admin/orders")
-public class AdminOrderController extends HttpServlet {
+public class AdminOrdersController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("com.internet.shop");
     private OrderService orderService = (OrderService) injector
             .getInstance(OrderService.class);
