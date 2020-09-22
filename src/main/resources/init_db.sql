9f0813668560cb9d5b07c3ca23cf5f1645b1d54c
Ilya <ilya03449@gmail.com>
2020-09-22 17:38:51 +0300

    created db

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -4,7 +4,7 @@ CREATE TABLE `internet_shop`.`products` (
   `product_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(225) NOT NULL,
   `price` DOUBLE NOT NULL DEFAULT 0,
-  `deleted` TINYINT(1) NULL DEFAULT 0,
+  `deleted` TINYINT(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`product_id`));
 
 INSERT INTO `products` (`name`, `price`)
@@ -12,3 +12,105 @@ VALUES ('DJI Phantom 4 PRO', '49470.00'),
         ('DJI Mavic 2 PR', '50720.00'),
         ('DJI Mavic PRO Platinum', '42710.00'),
         ('DJI Inspire 2', '142760.00');
+
+CREATE TABLE `internet_shop`.`roles` (
+  `role_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `role_name` VARCHAR(255) NOT NULL DEFAULT 'user',
+  PRIMARY KEY (`role_id`))
+  UNIQUE INDEX `role_name_UNIQUE` (`role_name` ASC) VISIBLE
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE `internet_shop`.`users` (
+  `user_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `login` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `deleted` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `login_UNIQUE` (`login` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE `internet_shop`.`users_roles` (
+  `user_id` BIGINT(11) NOT NULL,
+  `role_id` BIGINT(11) NOT NULL,
+  `deleted` TINYINT(1) NOT NULL DEFAULT 0,
+  INDEX `users_roles_isers.fk_idx` (`user_id` ASC) VISIBLE,
+  INDEX `users_roles_roles.fk_idx` (`role_id` ASC) VISIBLE,
+  CONSTRAINT `users_roles_users.fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `internet_shop`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `users_roles_roles.fk`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `internet_shop`.`roles` (`role_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE `internet_shop`.`orders` (
+  `order_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` BIGINT(11) NOT NULL,
+  `deleted` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`order_id`),
+  INDEX `order_user_id_users.fk_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `order_user_id_users.fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `internet_shop`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE `internet_shop`.`orders_products` (
+  `order_id` BIGINT(11) NOT NULL,
+  `product_id` BIGINT(11) NOT NULL,
+  INDEX `orders_products_order_id.fk_idx` (`order_id` ASC) VISIBLE,
+  INDEX `prders_products_product_id.fk_idx` (`product_id` ASC) VISIBLE,
+  CONSTRAINT `orders_products_order_id.fk`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `internet_shop`.`orders` (`order_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `prders_products_product_id.fk`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `internet_shop`.`products` (`product_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE `internet_shop`.`shopping_carts` (
+  `cart_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` BIGINT(11) NOT NULL,
+  `deleted` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`cart_id`),
+  INDEX `cart_user_id_users.fk_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `cart_user_id_users.fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `internet_shop`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE `internet_shop`.`shopping_carts_products` (
+  `cart_id` BIGINT(11) NOT NULL,
+  `product_id` BIGINT(11) NOT NULL,
+  INDEX `carts_products_shopping_carts.fk_idx` (`cart_id` ASC) VISIBLE,
+  INDEX `carts_products_products.fk_idx` (`product_id` ASC) VISIBLE,
+  CONSTRAINT `carts_products_shopping_carts.fk`
+    FOREIGN KEY (`cart_id`)
+    REFERENCES `internet_shop`.`shopping_carts` (`cart_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `carts_products_products.fk`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `internet_shop`.`products` (`product_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
